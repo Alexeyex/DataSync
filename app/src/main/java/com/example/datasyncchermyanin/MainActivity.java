d057b220ec85b3d2fba09c4c8813ad5fbeb650a5
Алексей МОП Чермянин <iamalive_spb@mail.ru>
2021-04-23 10:02:04 +0300

    update

--- app/src/main/java/com/example/datasyncchermyanin/MainActivity.java
+++ app/src/main/java/com/example/datasyncchermyanin/MainActivity.java
@@ -9,10 +9,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     private Button syncBtn;
@@ -25,39 +28,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         syncBtn = findViewById(R.id.syncBtn);
         res = findViewById(R.id.res);
-        formatForDate = new SimpleDateFormat("HH:mm:ss");
+        formatForDate = new SimpleDateFormat("HH", Locale.getDefault());
+        int time = Integer.parseInt(formatForDate.format(Calendar.getInstance().getTime()));
 
         syncBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_SYNC);
-                Date date = new Date();
-                try {
-                    Date d1 = formatForDate.parse("06:00:00");
-                    Calendar calendar1 = Calendar.getInstance();
-                    calendar1.setTime(d1);
-                    Date d2 = formatForDate.parse("14:00:00");
-                    Calendar calendar2 = Calendar.getInstance();
-                    calendar2.setTime(d2);
-                    Date d3 = formatForDate.parse("19:00:00");
-                    Calendar calendar3 = Calendar.getInstance();
-                    calendar3.setTime(d3);
-                    if (calendar2.after(date) && calendar1.before(date)) {
+                    if (time >= 6 && time < 14) {
                         intent.setData(Uri.parse("http://morning"));
-                        startActivity(intent);
-                    }
-                    if (calendar1.after(date) && calendar3.before(date)) {
+                    } else if (time >= 14 && time < 15) {
                         intent.setData(Uri.parse("http://afternoon"));
-                        startActivity(intent);
-                    }
-                    if (calendar3.after(date) && calendar1.before(date)) {
+                    } else {
                         intent.setData(Uri.parse("http://evening"));
-                        startActivity(intent);
                     }
-
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
             }
         });
     }

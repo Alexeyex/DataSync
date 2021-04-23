53b22be33a8863b7dacfd501dd13539f55013fd1
Alexey <60896220+Alexeyex@users.noreply.github.com>
2021-04-23 14:22:57 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/datasyncchermyanin/MainActivity.java
+++ app/src/main/java/com/example/datasyncchermyanin/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
                     } else {
                         intent.setData(Uri.parse("http://evening"));
                     }
+                startActivity(intent);
             }
         });
     }
